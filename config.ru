60dae128f9c2e2eeffa415ecf7aeaaf84f20f72b
Simon Bursell <simonbursell@gmail.com>
2011-05-07 17:18:36 -0700

    Working artist retrieval

--- config.ru
+++ config.ru
@@ -1,2 +1,2 @@
 require './soundtracks'
-run Sinatra::Application
+run Soundtracks
--- soundtracks.rb
+++ soundtracks.rb
@@ -1,10 +1,45 @@
-require 'sinatra'
+require "sinatra"
+require "httparty"
 
-get "/" do
-  erb :index
+class Lastfm
+  include HTTParty
+  base_uri "http://ws.audioscrobbler.com/2.0"
+
+
+  def initialize(api_key, username)
+    @api_key = api_key
+    @username = username
+  end
+
+  def top_artists
+    result = get_method("user.gettopartists")["topartists"]
+
+    # if a user does not exist or if a user exists but has no tracks
+    return [] if result.blank? || result["artist"].blank?
+
+    result["artist"]
+  end
+  
+  def get_method(method)
+    self.class.get("?method=#{method}&user=#{@username}&api_key=#{@api_key}")["lfm"]
+  end
 end
 
-get "/search" do
-  username = params[:username]
-  "Hi #{username}!"
+class Soundtracks < Sinatra::Base
+  set :root, File.expand_path(File.dirname(__FILE__))
+
+  configure do
+    @options = YAML.load_file(File.join(root, "soundtracks.yml"))
+  end
+
+  get "/" do
+    erb :index
+  end
+  
+  get "/search" do
+    lastfm_username = params[:username]
+    lastfm = Lastfm.new(@options["lastfm_key"], lastfm_username)
+    artists = lastfm.top_artists
+    erb :search, :locals => { :artists => artists, :username => lastfm_username }
+  end
 end
